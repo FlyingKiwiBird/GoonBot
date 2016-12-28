e474138386063b7e841124d46f3b74342dd77352
Drew McDermott <code@drewmcdermott.net>
2016-12-28 12:30:38 -0500

    Setup connection

--- main.js
+++ main.js
@@ -2,4 +2,22 @@ var xmpp = require('simple-xmpp');
 var request = require('request');
 var config = require('config');
 
-var user = 
+var user = config.get('jabber.user');
+var pass = config.get('jabber.password');
+var host = "goonfleet.com";
+var port = 5222;
+
+xmpp.connect({
+            jid: user,
+            password: password,
+            host: host,
+            port: port
+});
+
+xmpp.on('error', function(err) {
+        console.error(err);
+});
+
+xmpp.on('online', function(data) {
+    console.log('Connected: ' + data.jid.user);
+});
