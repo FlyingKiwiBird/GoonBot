5131f4969be14ede44b1ac9ea04c207493411297
Drew McDermott <code@drewmcdermott.net>
2016-12-28 13:14:56 -0500

    Connect and report messages

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 var xmpp = require('simple-xmpp');
-var request = require('request');
 var config = require('config');
 
 var user = config.get('jabber.user');
 var pass = config.get('jabber.password');
-var host = "goonfleet.com";
+var host = "conference.goonfleet.com";
 var port = 5222;
 
-xmpp.connect({
-            jid: user,
-            password: password,
-            host: host,
-            port: port
-});
+console.log("Connecting to " + host + ":" + port);
+
+
 
 xmpp.on('error', function(err) {
         console.error(err);
@@ -20,4 +16,19 @@ xmpp.on('error', function(err) {
 
 xmpp.on('online', function(data) {
     console.log('Connected: ' + data.jid.user);
+});
+
+xmpp.on('chat', function(from, message) {
+    console.log(from + ": " + message);
+    if(from == "directorbot@goonfleet.com")
+    {
+      console.log("PING!");
+    }
+});
+
+xmpp.connect({
+            jid: user,
+            password: pass,
+            host: host,
+            port: port
 });
